071a0dd5540c82b64648db60385f1f0d249e14cb
sharkbound <laptopblaster@gmail.com>
2018-12-02 15:18:27 -0800

    changed day1-part2 to use itertool.cycle instead of a while loop

--- day1/day1_part2.py
+++ day1/day1_part2.py
@@ -1,15 +1,16 @@
+from itertools import cycle
+
 with open('data.txt') as f:
     frequencies = [int(l) for l in f]
 
 current = 0
 seen = set()
 
-while current not in seen:
-    for freq in frequencies:
-        if current in seen:
-            break
+for freq in cycle(frequencies):
+    if current in seen:
+        break
 
-        seen.add(current)
-        current += freq
+    seen.add(current)
+    current += freq
 
 print(current)
