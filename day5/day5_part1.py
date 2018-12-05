2f8e9a3995c8bf003f40a1ca49edbc6f4e78f35b
sharkbound <laptopblaster@gmail.com>
2018-12-04 23:49:19 -0800

    day5-part1 solved

---
+++ day5/day5_part1.py
@@ -0,0 +1,20 @@
+with open('data.txt') as f:
+    data = f.read()
+
+
+def remove_negating_units(text):
+    inital_len = len(text)
+
+    for c in set(text):
+        text = text.replace(c.upper() + c.lower(), '').replace(c.lower() + c.upper(), '')
+
+    return inital_len - len(text), text
+
+
+while True:
+    changed, data = remove_negating_units(data)
+
+    if not changed:
+        break
+
+print(len(data))
