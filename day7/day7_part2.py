4a268cd35330b523729cb96c845c85bfa395621b
sharkbound <laptopblaster@gmail.com>
2018-12-25 19:41:20 -0800

    solved day7

--- day7/day7_part2.py
+++ day7/day7_part2.py
@@ -4,7 +4,51 @@ from operator import itemgetter
 from typing import Set
 from string import ascii_uppercase
 
-print(ascii_uppercase)
+FILE = 'data.txt'
+DELAY = 61
+MAX_JOBS = 5
+
+
+class AssemblyJobPool:
+    def __init__(self):
+        self.maxjobs = MAX_JOBS
+        self.runtime_ticks = 0
+        self.jobs = {}
+
+    @property
+    def has_free_job(self):
+        return len(self.jobs) < self.maxjobs
+
+    @property
+    def empty(self):
+        return not self.jobs
+
+    def add_job(self, data, ticks_to_complete):
+        if not self.has_free_job:
+            return
+
+        self.jobs[data] = [data, ticks_to_complete, 0]
+
+    def tick(self, step=1):
+        self.runtime_ticks += step
+        for job in self.jobs.values():
+            job[-1] += step
+
+    def remove_completed_jobs(self):
+        for j in [j for j, complete_ticks, active_ticks in self.jobs.values() if active_ticks >= complete_ticks]:
+            del self.jobs[j]
+            yield j
+
+    def __str__(self):
+        return ', '.join(
+            f'JOB{{{data} %{min(100, (active_ticks / complete_ticks) * 100):.0f} {active_ticks}/{complete_ticks}}}' for
+            data, complete_ticks, active_ticks in self.jobs.values())
+
+    def __len__(self):
+        return len(self.jobs)
+
+    def __contains__(self, item):
+        return item in self.jobs
 
 
 @dataclass
@@ -23,7 +67,7 @@ class Step:
 def parse_lines():
     ret = {}
 
-    with open('data.txt') as f:
+    with open(FILE) as f:
         for line in f:
             requirement_id, step_id = re.findall(r'\b\w\b', line)
 
@@ -41,18 +85,23 @@ def parse_lines():
 def solve():
     steps_done = set()
     steps_sorted = sorted(steps.items(), key=itemgetter(0))
-    order = []
+    pool = AssemblyJobPool()
 
-    while len(order) != len(steps_sorted):
+    while True:
+        steps_done.update(pool.remove_completed_jobs())
+
         for id, step in steps_sorted:
-            if id not in steps_done and step.all_requirements_done(steps_done):
-                steps_done.add(id)
-                order.append(id)
-                print(step.time_to_complete, step.id)
-                break
+            if id in steps_done or id in pool:
+                continue
 
-    return order
+            if step.all_requirements_done(steps_done):
+                pool.add_job(id, DELAY + ascii_uppercase.index(id))
+
+        if pool.empty:
+            return pool.runtime_ticks
+
+        pool.tick()
 
 
 steps = parse_lines()
-print(''.join(solve()))
+print(solve())
