54d1e6e42749c1f1e265e1106102d26694a2ec05
sharkbound <laptopblaster@gmail.com>
2018-12-28 14:30:08 -0800

    solved day8 part1

--- day8/day8_part1.py
+++ day8/day8_part1.py
@@ -1,16 +1,39 @@
+from __future__ import annotations
+
+from typing import List
+
 FILE = 'data.txt'
 
 
-def parse_lines():
-    with open(FILE) as f:
-        return list(map(int, f.read().split()))
+def parse_tree(iterator):
+    node = Node(child_count=next(iterator), metadata_count=next(iterator))
+
+    for _ in range(node.child_count):
+        node.children.append(parse_tree(iterator))
+
+    node.metadata.extend(next(iterator) for _ in range(node.metadata_count))
+    return node
 
 
-class Tree:
-    def __init__(self, value=None):
-        self.left = None
-        self.right = None
-        self.value = value
+class Node:
+    def __init__(self, child_count, metadata_count):
+        self.metadata_count = metadata_count
+        self.child_count = child_count
+        self.children: List[Node] = []
+        self.metadata: List[int] = []
+
+    def __repr__(self):
+        return f'<Node children={self.children} metadata={self.metadata}>'
+
+    def __iter__(self):
+        yield self
+
+        for child in self.children:
+            yield from child
 
 
-print(parse_lines())
+with open(FILE) as f:
+    numbers = tuple(map(int, f.read().split()))
+    tree = parse_tree(iter(numbers))
+
+print(sum(sum(node.metadata) for node in tree))
