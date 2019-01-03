c19f34ace5c15bd276d10f703cf6dee49e5dbb9a
sharkbound <laptopblaster@gmail.com>
2019-01-02 18:04:54 -0800

    solved day11 part1

---
+++ day11/day11_part1.py
@@ -0,0 +1,40 @@
+from operator import attrgetter
+
+import numpy as np
+from collections import namedtuple
+
+FILE = 'data.txt'
+
+with open(FILE) as f:
+    serial = int(next(f))
+
+
+def power_level(x, y, serial):
+    rack_id = x + 10
+    return int(((rack_id * y) + serial) * rack_id / 100 % 10) - 5
+
+
+Tile = namedtuple('Tile', 'x y power')
+
+powers = []
+grid = np.zeros((300, 300), dtype=int)
+
+for (y, x), _ in np.ndenumerate(grid):
+    power = power_level(x, y, serial)
+    powers.append(Tile(x, y, power))
+    grid[y, x] = power
+
+powers.sort(key=attrgetter('power'), reverse=True)
+best_tile = Tile(0, 0, -1)
+best_total = 0
+
+for tile in powers:
+    if tile.power <= 1:
+        break
+
+    surrounding_sum = grid[tile.y - 1:tile.y + 2, tile.x - 1:tile.x + 2].sum()
+    if surrounding_sum > best_total:
+        best_total = surrounding_sum
+        best_tile = tile
+
+print(f'{best_tile.x - 1},{best_tile.y - 1}')
