d46f2eefa2220e976c7f775dbbdf7447fd129817
sharkbound <laptopblaster@gmail.com>
2019-11-18 23:01:20 -0600

    trying to make a solution for day 5 part 1 using a deque

--- day5/day5_part1_optimized.py
+++ day5/day5_part1_optimized.py
@@ -1,16 +1,27 @@
 from collections import deque
 
 
+# https://github.com/fogleman/AdventOfCode2018/blob/master/5.py
+# def solve_part_1():
+#     data = read_data()
+#     res = ['']
+#
+#     for c in data:
+#         if c == res[-1].swapcase():
+#             res.pop()
+#         else:
+#             res.append(c)
+#
+#     return len(res) - 1
+
 def solve_part_1():
     data = deque(read_data())
-    while True:
-        for _ in range(len(data) + 2):
-            a, b = data.popleft(), data.popleft()
-            if a.lower() == b.lower() and a.islower() ^ b.islower():
-                break
-            data.extend((a, b))
-        else:
-            break
+    for _ in range(len(data)):
+        a, b = data.popleft(), data.popleft()
+        if a == b.swapcase():
+            continue
+        data.appendleft(b)
+        data.append(a)
     return len(data)
 
 
@@ -20,8 +31,8 @@ def read_data():
 
 
 # solve_part_1()
+print('actual :', solve_part_1(), '\ncorrect:', 9386)
 
-
-def test_part_1_is_correct():
-    ret = solve_part_1()
-    assert ret == 9386, f'day 5 part 1 answer should be 9386, actual {ret}'
+# def test_part_1_is_correct():
+#     ret = solve_part_1()
+#     assert ret == 9386, f'day 5 part 1 answer should be 9386, actual {ret}'
